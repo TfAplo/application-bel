789932fbfe464390ef41583539a6ac4cb2f9937e
basti <basti@192.168.1.61>
2023-12-26 19:10:42 +0100

    test correction classe

--- Application/src/metier/GenerateurFichierCSV.java
+++ Application/src/metier/GenerateurFichierCSV.java
@@ -1,4 +1,37 @@
+package metier;
 
-public class GenerateurFichierCSV {
+/**
+ * La classe GenerateurFichier est responsable de la génération des fichiers PNG à partir des données d'histogrammes
+ */
+public class GenerateurFichier 
+{
 
+    /**
+     * Constructeur du générateur de fichiers
+     */
+    public GenerateurFichier() 
+    {
+    }
+
+    /**
+     * Crée le fichier CSV à partir des données d'histogrammes
+     *
+     * @return le fichier CSV généré au format BufferedImage
+     */
+    public BufferedImage creerFichier() 
+    {
+        return null;
+    }
+
+    /**
+     * Getter de l'attribut fichier
+     * @return fichier de type BufferedImage
+     */
+	public BufferedImage getFichier() 
+	{
+		return null;
+	}
+
+    
+    
 }
