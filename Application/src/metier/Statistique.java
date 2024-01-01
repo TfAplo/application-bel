317e619cece21be4a44b1e7541d830235253bac9
flo <florianbiardeau@gmail.com>
2024-01-01 22:54:12 +0100

    Ajout refonte Tom

--- Application/src/metier/Statistique.java
+++ Application/src/metier/Statistique.java
@@ -1,76 +1,13 @@
 package metier;
 
-import java.util.*;
-
-/**
- * Permet l'instanciation de statistique d'une particule trouvée en analysant une image
- */
 public class Statistique {
-
-    /**
-     * Default constructor
-     */
-    public Statistique() {
-    }
-
-    /**
-     * 
-     */
-    private int idStatistique;
-
-    /**
-     * 
-     */
-    private double surfaceParticulePx;
-
-    /**
-     * 
-     */
-    private double coCointHautGaucheX;
-
-    /**
-     * 
-     */
-    private double coCointHautGaucheY;
-
-    /**
-     * 
-     */
-    private double coCointHautDroitX;
-
-    /**
-     * 
-     */
-    private double coCointHautDroitY;
-
-    /**
-     * 
-     */
-    private double coCentreX;
-
-    /**
-     * 
-     */
-    private double coCentreY;
-
-    /**
-     * 
-     */
-    private double orientation;
-
-    /**
-     * 
-     */
-    private double longueurAxeMajeurPx;
-
-    /**
-     * 
-     */
-    private double longueurAxeMineurPx;
-
-    /**
-     * 
-     */
-    private double diametreEquivalentPx;
-
+	private int idStatistique;
+	private int nbParticulesTrouvees;
+	private double ratioSurfaceCouverte;
+	private double moyenneAires;
+	private double moyenneDiametresEquivalents;
+	private double ecartTypeAires;
+	private double ecartTypeDiametreEquivalent;
+	private int idImage;
+	
 }
