fd0ee5c1e39fe16a4a315266d4278a42666a8faa
valen <valen@DESKTOP-0CDVB5O.home>
2024-01-05 16:41:38 +0100

    si on supprime l'image dans la section image selectionner comme l'affichage du dessus n'est pas mit a jour cela peux causer des pbs

--- Application/src/controleurs/ControleRecherche.java
+++ Application/src/controleurs/ControleRecherche.java
@@ -1,91 +1,94 @@
 package controleurs;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Iterator;
 
 import dao.ImageDAO;
 import javafx.scene.control.CheckBox;
-import javafx.scene.control.Hyperlink;
+import javafx.scene.control.Label;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import metier.Image;
-import metier.ImageRecherche;
-/**
- * Cette classe controle les autres.
- */
-public class ControleRecherche{
-	
-	ImageDAO imageDao = new ImageDAO();
 
+public class ControleRecherche {
+
+	ImageDAO imageDao = new ImageDAO();
+	
+	private ArrayList<Image> listeImage = new ArrayList<>();
+	
 	/*
 	 * Liste d'image Selectionner
 	 */
 	private ArrayList<Image> listeImageSelectionner = new ArrayList<>();
-
-
-    /**
-     * Default constructor
-     */
-    public ControleRecherche(){
-    }
-
-	/**
-     * @param nom, valeur de l'input de la recherche , censé contenir un potentiel nom d'image.
-     */
-    public void recherche(String recherche, VBox afficherResultatContainer) {
+	
+	
+	public void recherche(String recherche, VBox afficherResultatContainer, VBox imageSelected) 
+	{
+		// recuperer une liste d'image qui correspondent dans la bdd
+		listeImage= imageDao.lire(recherche);
+		
+		//Si la liste n'est pas vide alors on affiche les resultats, sinon la liste est vide donc on vide le container
+		if(!listeImage.isEmpty())
+		{
+			affichage(listeImage, afficherResultatContainer,imageSelected);
+		} else 
+		{
+			afficherResultatContainer.getChildren().clear();
+		}
+	}
+	
+	public void affichage(ArrayList<Image> listeImage, VBox container, VBox container2)
+	{
+		container.getChildren().clear();
+		//parcourir les elements
+		for (Image image: listeImage)
+		{
+			// creer l'element hbox contenant le nom et une checkbox pour le selection
+			HBox elementImage = creerElement(image, container2);
+			//ajouter l'element au container 
+			container.getChildren().add(elementImage);
+		}
+		
+	}
+	/* creer l'element hbox contenant le nom et une checkbox pour une image
+	 * @param image, objet Image
+	 * @return HBox
+	 */
+	public HBox creerElement(Image image, VBox container)
+	{
+		HBox elementImage = new HBox();
+		//creer un Label fxml pour l'afficher le nom de l'image
+		Label nomImage = new Label(image.getNomImage());
+		// Creer une checkbox pour pouvoir effectuer une selection d'image
+		CheckBox CheckBox = new CheckBox();
+		
+		CheckBox.setOnAction(e -> actualiserSelection(image, container));
+		
+		//check si l'image est deja dans la liste (et si on doit "check" la box)
+		for (Image imageSelectionner: listeImageSelectionner)
+		{
+			if(isEqual(imageSelectionner, image)) 
+			{
+				CheckBox.setSelected(true);
+			}
+		}
+		
+		// ajouter les elements au container
+		elementImage.getChildren().add(nomImage);
+		elementImage.getChildren().add(CheckBox);
+		
+		// ajouter un id pour le css
+		elementImage.setId("hContainer");
+		
+		return elementImage;
+	}
+	
+	public void actualiserSelection(Image image, VBox container)
+	{
+		boolean isRemoved = false;
+    	Iterator<Image> iterator = listeImageSelectionner.iterator();
     	
-        //recuperer une liste d'image qui correspondent
-        ArrayList<Image> listeImage = imageDao.lire(recherche);
-        
-        // les afficher sur la fenetre , On verifie que la liste ne soit pas vide 
-    	if(!listeImage.isEmpty()) {
-    		// liste non vide, on clear le container pour pouvoir mettre les nouveaux resultats correspondent 
-    		afficherResultatContainer.getChildren().clear();
-    		//parcourir la liste des images trouvé qui correspondent a la recherche.
-        	for(Image image : listeImage) {
-        		// creation de l'objet ImageRecherche qui contient l'objet image + un boolean sur la selection
-        		ImageRecherche imageRechercher = new ImageRecherche(image,false);
-        		
-        		//pour chaque image on creer un Hyperlink fxml pour l'afficher et l'ajouter sur la fenetre
-        		Hyperlink hyperlink = new Hyperlink(image.getNomImage());
-        		
-        		// Creer une checkbox pour pouvoir effectuer une selection d'image
-        		CheckBox hyperlinkCheckBox = new CheckBox();
-        
-        		hyperlinkCheckBox.setOnAction(e -> ajoutImageSelectionner(image));
-        		//verifier que l'image est dans la liste ou non (pb lorsqu'on recherche des images deja dans la liste la checkbox est reinitialiser
-        		// et donc cela ne correspond plus a son etat dans la liste
-        		ArrayList<Image> liste = getListeImageSelectionner();
-        		
-        		for(Image imageSelect: liste) {
-        			if(isEqual(imageSelect, image)) {
-        				hyperlinkCheckBox.setSelected(true);
-        			}
-        		}    		
-        		
-        		//Creer un element qui contient le nom de l'image et la checkbox
-        		HBox hContainer = new HBox();
-        		hContainer.setId("hContainer");
-        		hContainer.getChildren().add(hyperlink);
-        		hContainer.getChildren().add(hyperlinkCheckBox);
-        		
-        		//ajouter les elements au container
-        		afficherResultatContainer.getChildren().add(hContainer);
-        		
-        	}
-    	} else {
-    		// ici la liste est donc vide on peux clear le container
-    		afficherResultatContainer.getChildren().clear();
-    	}
-    }
-    
-    /* ajoute l'image ou l'enleve en fonction de sa presence ou non dans la liste
-     * @param image, instance de Image
-     */
-    public void ajoutImageSelectionner(Image image) 
-    {
-    	boolean isRemoved = false;
-    	Iterator<Image> iterator = getListeImageSelectionner().iterator();
-    	
+    	//si l'image est deja dans la liste alors on la retire sinon on l'ajoute
     	while(iterator.hasNext()) 
     	{
     		Image imageSelect = iterator.next();
@@ -93,22 +96,25 @@ public class ControleRecherche{
     		{
     			isRemoved = true;
     			iterator.remove();
+    			affichage(listeImageSelectionner, container, null);
+    			//actualiser les checkborecherches
+    			
+    			//affichage(listeImage, container2, null);
+    			
+    			
+    			System.out.println("remove");
     		}
     	}
     	if(!isRemoved) 
     	{
     		listeImageSelectionner.add(image);
+    		container.getChildren().add(creerElement(image,container));
+    		System.out.println("add");
+    		
     	}
     	System.out.println(listeImageSelectionner);
-    }
-    
-    /*
-     * retourne la liste d'image selectionner
-     */
-    public ArrayList<Image> getListeImageSelectionner() {
-		return listeImageSelectionner;
 	}
-    
+	
     /*
      * Compare si deux image sont eguale (avec leurs id qui est unique)
      */
@@ -121,5 +127,13 @@ public class ControleRecherche{
         }
         return false;
     }
-              
+	
+	/*
+     * retourne la liste d'image selectionner
+     */
+    public ArrayList<Image> getListeImageSelectionner() {
+		return listeImageSelectionner;
+	}
+    
+
 }
